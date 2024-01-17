5488c34284565c3f2eba1217c3ed80f47b9a132c
Amit <119887692+layogtima@users.noreply.github.com>
2024-01-18 01:19:52 +0530

    Updated the SV website with a baseline page for the events page, tweaked the colors and font-sizes for everything.

---
+++ flow-carnival/HTML/source-files/oomph-js-files/map.js
@@ -0,0 +1,217 @@
+// ------------------------------------------------
+// Project Name: Oomph - Halloween Style Coming Soon & Landing Page Template
+// Project Description: Oomph - awesome coming soon template to kick-start your project
+// Tags: mix_design, oomph, coming soon, under construction, template, coming soon page, html5, css3
+// Version: 3.0.0
+// Build Date: December 2020
+// Last Update: October 2022
+// This product is available exclusively on Themeforest
+// Author: mix_design
+// Author URI: https://themeforest.net/user/mix_design
+// File name: map.js
+// ------------------------------------------------
+
+// ------------------------------------------------
+// Table of Contents
+// ------------------------------------------------
+//
+//  1. Google Map Parameters
+//  2. Google Map Custom Marker Icon
+//  3. Style Of The Map
+//  4. Google Map Options
+//  5. Inizialize The Map
+//  6. Custom Marker
+//  7. Custom zoom-in/zoom-out Buttons
+//
+// ------------------------------------------------
+// Table of Contents End
+// ------------------------------------------------
+
+
+$(function() {
+  // Insert Your Google Map Parameters
+  var latitude = 40.761425,
+    longitude = -73.977643,
+    map_zoom = 14;
+
+  // Google Map Custom Marker Icon
+  var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
+  var marker_url = ( is_internetExplorer11 ) ? 'img/icons/location.png' : 'img/icons/location.svg';
+
+    var main_color = '#160c37',
+      saturation_value= 1,
+      brightness_value= 5;
+
+      // Style Of The Map
+      var style= [
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#f5e9e6"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#7f7d7a"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.land_parcel",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#9fab9b"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#81907a"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#d7c6c1"
+            }
+          ]
+        },
+        {
+          "featureType": "road.arterial",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#e5d0cd"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#f47b7b"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#d84646"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway.controlled_access",
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#e14141"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway.controlled_access",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#c83737"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#cfc3b9"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9a897e"
+            }
+          ]
+        }
+      ];
+
+      // Google Map Options
+      var map_options = {
+            center: new google.maps.LatLng(latitude, longitude),
+            zoom: map_zoom,
+            gestureHandling: 'cooperative',
+            panControl: false,
+            zoomControl: false,
+            mapTypeControl: false,
+            streetViewControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: false,
+            styles: style,
+        }
+
+      // Inizialize The Map
+      var map = new google.maps.Map(document.getElementById('google-container'), map_options);
+
+      // Custom Marker
+      var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(latitude, longitude),
+          map: map,
+          visible: true,
+        icon: marker_url,
+      });
+
+      // Custom zoom-in/zoom-out Buttons
+      function CustomZoomControl(controlDiv, map) {
+
+          var controlUIzoomIn= document.getElementById('zoom-in'),
+            controlUIzoomOut= document.getElementById('zoom-out');
+          controlDiv.appendChild(controlUIzoomIn);
+          controlDiv.appendChild(controlUIzoomOut);
+
+        google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
+            map.setZoom(map.getZoom()+1)
+        });
+        google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
+            map.setZoom(map.getZoom()-1)
+        });
+      }
+
+      var zoomControlDiv = document.createElement('div');
+      var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+
+        map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(zoomControlDiv);
+
+});
